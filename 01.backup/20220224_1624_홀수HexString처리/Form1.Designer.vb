301929f675b16f9ace705e777ccb7c38db8beab0
ymcajudo <ymcajudo@gmail.com>
2022-11-23 18:51:56 +0900

    초기 커밋.

---
+++ 01.backup/20220224_1624_홀수HexString처리/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tb_InputData = New System.Windows.Forms.TextBox()
+        Me.rb_ASCII = New System.Windows.Forms.RadioButton()
+        Me.rb_Hex = New System.Windows.Forms.RadioButton()
+        Me.bt_CRC32 = New System.Windows.Forms.Button()
+        Me.tb_CRCResult = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'tb_InputData
+        '
+        Me.tb_InputData.Location = New System.Drawing.Point(144, 23)
+        Me.tb_InputData.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.tb_InputData.Multiline = True
+        Me.tb_InputData.Name = "tb_InputData"
+        Me.tb_InputData.Size = New System.Drawing.Size(526, 252)
+        Me.tb_InputData.TabIndex = 0
+        '
+        'rb_ASCII
+        '
+        Me.rb_ASCII.AutoSize = True
+        Me.rb_ASCII.Location = New System.Drawing.Point(172, 341)
+        Me.rb_ASCII.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rb_ASCII.Name = "rb_ASCII"
+        Me.rb_ASCII.Size = New System.Drawing.Size(59, 20)
+        Me.rb_ASCII.TabIndex = 1
+        Me.rb_ASCII.TabStop = True
+        Me.rb_ASCII.Text = "ASCII"
+        Me.rb_ASCII.UseVisualStyleBackColor = True
+        '
+        'rb_Hex
+        '
+        Me.rb_Hex.AutoSize = True
+        Me.rb_Hex.Location = New System.Drawing.Point(172, 299)
+        Me.rb_Hex.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rb_Hex.Name = "rb_Hex"
+        Me.rb_Hex.Size = New System.Drawing.Size(48, 20)
+        Me.rb_Hex.TabIndex = 2
+        Me.rb_Hex.TabStop = True
+        Me.rb_Hex.Text = "Hex"
+        Me.rb_Hex.UseVisualStyleBackColor = True
+        '
+        'bt_CRC32
+        '
+        Me.bt_CRC32.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt_CRC32.Location = New System.Drawing.Point(495, 299)
+        Me.bt_CRC32.Name = "bt_CRC32"
+        Me.bt_CRC32.Size = New System.Drawing.Size(175, 64)
+        Me.bt_CRC32.TabIndex = 3
+        Me.bt_CRC32.Text = "CRC-32"
+        Me.bt_CRC32.UseVisualStyleBackColor = True
+        '
+        'tb_CRCResult
+        '
+        Me.tb_CRCResult.Location = New System.Drawing.Point(279, 340)
+        Me.tb_CRCResult.Name = "tb_CRCResult"
+        Me.tb_CRCResult.Size = New System.Drawing.Size(162, 23)
+        Me.tb_CRCResult.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(28, 140)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 19)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Input Data"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(279, 301)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(168, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "CRC-32 result(Hex value)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(419, 378)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(251, 14)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Copyright 2022. Myeongjin Kim. All rights reserved."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(706, 410)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_CRCResult)
+        Me.Controls.Add(Me.bt_CRC32)
+        Me.Controls.Add(Me.rb_Hex)
+        Me.Controls.Add(Me.rb_ASCII)
+        Me.Controls.Add(Me.tb_InputData)
+        Me.Font = New System.Drawing.Font("Arial", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "CRC-32 Calculator by MJ Kim"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tb_InputData As TextBox
+    Friend WithEvents rb_ASCII As RadioButton
+    Friend WithEvents rb_Hex As RadioButton
+    Friend WithEvents bt_CRC32 As Button
+    Friend WithEvents tb_CRCResult As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ 01.backup/20220224_1624_홀수HexString처리/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+    Dim SelectHex As Boolean = False
+    Dim SelectASCII As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rb_Hex.Checked = True           'CRC32 계산은 기본적으로 Hex로 선택
+
+    End Sub
+
+    Private Sub bt_CRC32_Click(sender As Object, e As EventArgs) Handles bt_CRC32.Click
+
+        Dim InputData As String = tb_InputData.Text.Trim()
+
+        Try
+            If SelectHex = True Then
+
+                '입력된 Hex string의 Length가 홀수이면 맨 마지막 hex 값 앞에 0을 추가. 예: 12345 => 123405
+                If Int(InputData.Length) Mod 2 <> 0 Then
+                    InputData = InputData.Substring(0, InputData.Length - 1) & "0" & InputData.Substring(InputData.Length - 1, 1)
+                End If
+
+                Dim tmpary As Byte() = New Byte((InputData.Length / 2) - 1) {}
+
+                Dim i As Integer
+
+                For i = 0 To tmpary.Length - 1
+                    tmpary(i) = Convert.ToByte(InputData.Substring((i * 2), 2), &H10)     'char 2개를 각각 Hex 값으로 변환 후 하나(1byte)로 합침, 예: 01 => 0x01, 0A => 0x0A, 86 => 0x86, E9 => 0xE9
+                Next i
+
+                tb_CRCResult.Text = Hex(Crc32.ComputeChecksum(tmpary))
+
+            ElseIf SelectASCII = True Then
+                tb_CRCResult.Text = Hex(Crc32.ComputeChecksum(Encoding.ASCII.GetBytes(InputData)))
+            Else
+                MessageBox.Show("Please, select Hex or ASCII")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Please, check the input data. It should be Hex string and the length of it should be also an even number.")
+        End Try
+
+    End Sub
+
+    Public Class Crc32
+        'CRC32 변환 클래스
+        Shared table As UInteger()
+
+        Shared Sub New()
+            Dim poly As UInteger = &HEDB88320UI     'Polynomial value   : 04C11DB7 (Mirror value = EDB88320) 'VB.Net code에선 Mirror value 사용
+            table = New UInteger(255) {}
+            Dim temp As UInteger = 0
+            For i As UInteger = 0 To table.Length - 1
+                temp = i
+                For j As Integer = 8 To 1 Step -1
+                    If (temp And 1) = 1 Then
+                        temp = CUInt((temp >> 1) Xor poly)
+                    Else
+                        temp >>= 1
+                    End If
+                Next
+                table(i) = temp
+            Next
+        End Sub
+
+        Public Shared Function ComputeChecksum(bytes As Byte()) As UInteger
+            Dim crc As UInteger = &HFFFFFFFFUI      'Mirror algorithm을 사용해서 Initialise가 &H0인데 &HFFFFFFFF로 표기
+            For i As Integer = 0 To bytes.Length - 1
+                Dim index As Byte = CByte(((crc) And &HFF) Xor bytes(i))
+                crc = CUInt((crc >> 8) Xor table(index))
+            Next
+            Return Not crc
+        End Function
+    End Class
+
+    Private Sub rb_Hex_CheckedChanged(sender As Object, e As EventArgs) Handles rb_Hex.CheckedChanged
+        SelectHex = True
+        SelectASCII = False
+
+    End Sub
+
+    Private Sub rb_ASCII_CheckedChanged(sender As Object, e As EventArgs) Handles rb_ASCII.CheckedChanged
+        SelectASCII = True
+        SelectHex = False
+
+    End Sub
+End Class
